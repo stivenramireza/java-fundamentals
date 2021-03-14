c6e3239dbbe75742c1f3e88b63d88bef741a0a9b
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-03-14 18:34:59 -0500

    Building strong foundations

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java Learning");
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        boolean hasPerfectAttendance = true;
+
+        String studentFirstName = "Kayla";
+        String studentLastName = "Hammon";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(hasPerfectAttendance);
+
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        System.out.println("What do you want to update it to?");
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
     }
 
 }
