dafed59111c6e62062eb3d4e0eab634f9ad406d8
Stiven Ramírez Arango <stivenramireza@gmail.com>
2021-03-20 21:49:29 -0500

    Functions chapter finished

--- src/functions/Main.java
+++ src/functions/Main.java
@@ -11,7 +11,21 @@ public class Main {
         System.out.println("It's developer tea time!");
     }
 
+    public static double calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        return result;
+    }
+
+    public static double calculateEmployeeSalary(int hoursPerWeek, double moneyPerHour, int vacationDays) {
+        if (hoursPerWeek < 0 || moneyPerHour < 0 || vacationDays < 0) return -1;
+        double unpaidTime = vacationDays * moneyPerHour * 8;
+        return (hoursPerWeek * moneyPerHour * 52) - unpaidTime;
+    }
+
     public static void main(String[] args) {
+        // Calling functions
         System.out.println("Welcome to your new job");
 
         announceDeveloperTeaTime();
@@ -22,6 +36,25 @@ public class Main {
         announceDeveloperTeaTime();
 
         System.out.println("Get promoted!");
+
+        // Parameters
+        calculateTotalMealPrice(15, .2, .08);
+        calculateTotalMealPrice(25, .18, .08);
+
+        // Return types
+        double groupTotalMealPrice = calculateTotalMealPrice(100, .2, .05);
+        System.out.println(groupTotalMealPrice);
+
+        double individualMealPrice = groupTotalMealPrice / 5;
+        System.out.println(individualMealPrice);
+
+        // Built-in functions
+        double result = Math.pow(2, 5);
+        System.out.println(result);
+
+        // Challenge (salary calculator)
+        double employeeYearlySalary = calculateEmployeeSalary(40, 15, 8);
+        System.out.println(employeeYearlySalary);
     }
 
 }
